4d700419c847d0bb26659846d8cc8db870cc2450
sajero <sajero@users.noreply.github.com>
2024-08-08 22:45:41 -0600

    feat: persistencia sin utilizar una base de datos by Brenda Karol Ibarra C

--- Owner avatar ibarra-brendakarol_contreras-gustavoadolfo/src/main.java
+++ Owner avatar ibarra-brendakarol_contreras-gustavoadolfo/src/main.java
@@ -8,7 +8,6 @@ import servicio.ODONTOLOGO_SERVICE;
 public class main {
     public static void main(String[] args) {
         ODONTOLOGO_SERVICE serviceDB = new ODONTOLOGO_SERVICE(new ODONTOLOGO_DAO_H2());
-        ODONTOLOGO_SERVICE serviceCollection = new ODONTOLOGO_SERVICE(new ODONTOLOGO_DAO_COLLECTION());
 
         ODONTOLOGO odontologo1 = new ODONTOLOGO("12345", "Juan", "Perez");
         ODONTOLOGO odontologo2 = new ODONTOLOGO("67890", "Ana", "Gomez");
@@ -16,13 +15,10 @@ public class main {
         serviceDB.guardarOdontologo(odontologo1);
         serviceDB.guardarOdontologo(odontologo2);
 
-        serviceCollection.guardarOdontologo(odontologo1);
-        serviceCollection.guardarOdontologo(odontologo2);
 
         System.out.println("Odontologos desde la base de datos:");
         serviceDB.listarTodosLosOdontologos().forEach(o -> System.out.println(o.getNombre() + " " + o.getApellido()));
 
         System.out.println("Odontologos desde la colección:");
-        serviceCollection.listarTodosLosOdontologos().forEach(o -> System.out.println(o.getNombre() + " " + o.getApellido()));
-    }
+     }
 }
