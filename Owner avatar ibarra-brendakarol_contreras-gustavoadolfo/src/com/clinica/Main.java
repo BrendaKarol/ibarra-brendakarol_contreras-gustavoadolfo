40773a634f317378d18308a271a82b3aac116b26
sajero <sajero@users.noreply.github.com>
2024-08-08 22:44:53 -0600

    feat: persistencia sin utilizar una base de datos by Brenda Karol Ibarra C

--- Owner avatar ibarra-brendakarol_contreras-gustavoadolfo/src/com/clinica/Main.java
+++ Owner avatar ibarra-brendakarol_contreras-gustavoadolfo/src/com/clinica/Main.java
@@ -15,7 +15,7 @@ import org.apache.log4j.BasicConfigurator;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         BasicConfigurator.configure();
 
         DAO<Odontologo> odontologoDAO = new OdontologoDAOArrayList();
